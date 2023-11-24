7162fbde0a25a14c3ff0e48134119c5d367a0e88
Moises Correa <145796144+MoicoeMT@users.noreply.github.com>
2023-11-24 03:09:30 +0000

    committwo

--- test.js
+++ test.js
@@ -1,11 +1,8 @@
 const { sum } = require('./app.js');
 
-// Comienza tu primera prueba
-test('adds 14 + 9 to equal 23', () => {
-    // Dentro de la prueba llamamos a nuestra función sum con 2 números
-    let total = sum(14, 9);
 
-    // Esperamos que la suma de esos 2 números sea 23
+test('adds 14 + 9 to equal 23', () => {
+    let total = sum(14, 9);
     expect(total).toBe(23);
 });
 
